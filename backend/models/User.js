c8122fa35db955d7e8131f6ff21ab0cb44f00cfa
bugdebug29 <bug.d.bug29@gmail.com>
2022-12-23 12:05:38 +0530

    adding isVerified field into User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,6 +29,10 @@ var UserSchema = new mongoose.Schema(
       enum: ["user", "admin"],
       default: "user"
     },
+    isVerified: {
+      type: Boolean,
+      default: false
+    },
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
