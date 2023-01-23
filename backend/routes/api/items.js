9d849d8f0c266d50ccbfd06ac28c0fd1390a091f
bugdebug29 <bug.d.bug29@gmail.com>
2023-01-23 16:42:56 +0000

    _

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -7,6 +7,7 @@ var auth = require("../auth");
 const { sendEvent } = require("../../lib/event");
 
 const { Configuration, OpenAIApi} = require("openai");
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
