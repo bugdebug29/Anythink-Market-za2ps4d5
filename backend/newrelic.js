1af84981dd4edd5b8470d5b861183c527e60361c
WilcoApp <hello@wilco.gg>
2023-01-25 10:20:22 +0000

    Watching the Watchman

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -6,10 +6,6 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  /**
-   * Array of application names.
-   */
-  app_name: ['Anythink-Market-Za2'],
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
@@ -24,6 +20,14 @@ exports.config = {
    * attributes include/exclude lists.
    */
   allow_all_headers: true,
+  application_logging: {
+    forwarding: {
+      /**
+       * Toggles whether the agent gathers log records for sending to New Relic.
+       */
+      enabled: true
+    }
+  },
   attributes: {
     /**
      * Prefix of attributes to exclude from all destinations. Allows * as wildcard
@@ -31,7 +35,7 @@ exports.config = {
      *
      * NOTE: If excluding headers, they must be in camelCase form to be filtered.
      *
-     * @name NEW_RELIC_ATTRIBUTES_EXCLUDE
+     * @env NEW_RELIC_ATTRIBUTES_EXCLUDE
      */
     exclude: [
       'request.headers.cookie',
