f12630819608d9c22a56437c7e9e77630c0b43ee
bugdebug29 <bug.d.bug29@gmail.com>
2023-01-25 06:54:51 +0000

    removing license key field

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -10,10 +10,6 @@ exports.config = {
    * Array of application names.
    */
   app_name: ['Anythink-Market-Za2'],
-  /**
-   * Your New Relic license key.
-   */
-  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
